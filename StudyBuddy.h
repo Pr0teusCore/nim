37284680cfacdbeca172d54aa54b347abf54f0b7
sasquatch7dwarf <156696624+sasquatch7dwarf@users.noreply.github.com>
2025-04-27 14:57:17 -0500

    Update StudyBuddy.h

--- StudyBuddy.h
+++ StudyBuddy.h
@@ -1,27 +1,30 @@
 #pragma once
 
-#define MAX_NAME         50
-#define MAX_COURSENAME   10
-#define MAX_SERVERS      100
-#define DEFAULT_BUFLEN   512
-#define DEFAULT_PORT     29333
-#define Study_WHERE      "Where?"
-#define Study_LOC	     "Loc="
-#define Study_WHAT       "What?"
-#define Study_COURSES    "Courses="
-#define Study_MEMBERS    "Members?"
-#define Study_MEMLIST    "Members="
-#define Study_CONFIRM    "YES"
-#define Study_DENY       "NO"
-#define MAX_BUFFER 81
-#define TIMEOUT_SECONDS 30
-#define DISCOVERY_TIMEOUT 2
-#define CHALLENGE_TIMEOUT 10
-#define GREAT_TIMEOUT 2
-#define MAX_SERVERS 100
-#define QUERY "Who?"
-#define NAME_PREFIX "Name="
-#define PLAYER_PREFIX "Player="
+#include <vector>
+using namespace std;
+
+#define MAX_NAME         	50
+#define MAX_COURSENAME   	10
+#define MAX_SERVERS      	100
+#define DEFAULT_BUFLEN   	512
+#define DEFAULT_PORT     	29333
+#define Study_WHERE      	"Where?"
+#define Study_LOC	     	"Loc="
+#define Study_WHAT       	"What?"
+#define Study_COURSES    	"Courses="
+#define Study_MEMBERS    	"Members?"
+#define Study_MEMLIST    	"Members="
+#define Study_CONFIRM    	"YES"
+#define Study_DENY       	"NO"
+#define MAX_BUFFER 		81
+#define TIMEOUT_SECONDS 	30
+#define DISCOVERY_TIMEOUT 	2
+#define CHALLENGE_TIMEOUT 	10
+#define GREAT_TIMEOUT 		2
+#define MAX_SERVERS 		100
+#define QUERY 			"Who?"
+#define NAME_PREFIX 		"Name="
+#define PLAYER_PREFIX 		"Player="
 
 struct ServerStruct {
 	char name[MAX_NAME];
